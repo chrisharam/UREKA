d6446c77a719139559a7f8a98b69bd64cabd3ee4
haram Jeong <haram001021@gmail.com>
2025-08-28 16:19:57 +0900

    2025_08_28

---
+++ Ureka_class/javaScript/practice_01/test.js
@@ -0,0 +1,13 @@
+function a(){   
+            alert('hello^^');
+            console.log('hi');
+            document.getElementById('demo').innerHTML="안녕";
+        }
+
+
+let x, y, z;
+x = 5;
+y = 6;
+z = x+ y;
+document.write(z*2);
+console.log(z*2)
